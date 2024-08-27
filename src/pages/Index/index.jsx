b859e21be03805235bcb3f037d7d9b7e6f02153c
cheeseburgertony <1821955020@qq.com>
2024-08-27 17:55:05 +0800

    配置 CSS 预处理器 Less

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,9 +1,12 @@
 import { memo } from 'react'
 import { Button } from 'zarm'
 
+import s from './style.module.less'
+
 const Index = memo(() => {
   return (
-    <div>
+    <div className={s.index}>
+      <span>样式</span>
       Index
       <Button theme='primary'>按钮</Button>
     </div>
