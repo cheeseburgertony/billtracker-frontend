21437d6422919c8a6cb8bb5642b2ab97129f2b5c
cheeseburgertony <1821955020@qq.com>
2024-08-27 22:53:25 +0800

    feat:底部导航栏-底部导航栏的显示和隐藏

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useRoutes } from 'react-router-dom'
+import { useLocation, useRoutes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 import routes from '@/router'
 import NavBar from '@/components/NavBar'
 
 function App() {
+  const [showNav, setShowNav] = useState(false)
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    // pathname一改变看pathname是否需要NavBar从而来控制显示和隐藏NavBar
+    const needNav = ['/', '/data', '/user']
+    setShowNav(needNav.includes(pathname))
+  }, [pathname])
 
   return (
     <>
       {useRoutes(routes)}
-      <NavBar showNav={true} />
+      <NavBar showNav={showNav} />
     </>
   )
 }
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,13 @@
 import Data from "@/pages/Data"
+import Detail from "@/pages/Detail"
 import Home from "@/pages/Home"
 import User from "@/pages/User"
 
 const routes = [
   { path: '/', element: <Home /> },
   { path: '/data', element: <Data /> },
-  { path: '/user', element: <User /> }
+  { path: '/user', element: <User /> },
+  { path: '/detail', element: <Detail /> }
 ]
 
 export default routes
