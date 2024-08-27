81c33426b49b58386465ae2cac3537bc925a30bc
cheeseburgertony <1821955020@qq.com>
2024-08-27 22:36:59 +0800

    feat:底部导航栏-添加底部导航图标

---
+++ src/components/CustomIcon/index.jsx
@@ -0,0 +1,5 @@
+import { Icon } from "zarm"
+
+const CustomIcon = Icon.createFromIconfont('//at.alicdn.com/t/font_2236655_w1mpqp7n1ni.js')
+
+export default CustomIcon
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { TabBar } from 'zarm'
 import { useNavigate } from 'react-router-dom'
 import s from './style.module.less'
+import CustomIcon from '../CustomIcon'
 
 const NavBar = memo(({ showNav }) => {
   const [activeKey, setActiveKey] = useState('/')
@@ -16,9 +17,9 @@ const NavBar = memo(({ showNav }) => {
   return (
     <>
       <TabBar visible={showNav} className={s.tab} activeKey={activeKey} onChange={value => handleChange(value)}>
-        <TabBar.Item itemKey='/' title='账单' />
-        <TabBar.Item itemKey='/data' title='统计' />
-        <TabBar.Item itemKey='/user' title='我的' />
+        <TabBar.Item itemKey='/' title='账单' icon={<CustomIcon type='zhangdan' />} />
+        <TabBar.Item itemKey='/data' title='统计' icon={<CustomIcon type='tongji' />} />
+        <TabBar.Item itemKey='/user' title='我的' icon={<CustomIcon type='wode' />} />
       </TabBar >
     </>
   )
