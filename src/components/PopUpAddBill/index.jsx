8aad7769a41dce79f9150fc1b473de0d439bd7eb
cheeseburgertony <1821955020@qq.com>
2024-08-28 22:16:28 +0800

    feat: 账单列表页-添加账单-账单类型选择和账单时间选择实现

--- src/components/PopUpAddBill/index.jsx
+++ src/components/PopUpAddBill/index.jsx
@@ -1,15 +1,24 @@
-import { forwardRef, memo, useImperativeHandle, useState } from 'react'
+import { forwardRef, memo, useImperativeHandle, useRef, useState } from 'react'
 
 import s from './style.module.less'
-import { Popup } from 'zarm'
+import { Icon, Popup } from 'zarm'
+import classNames from 'classnames'
+import PopupDate from '../PopupDate'
+import dayjs from 'dayjs'
 
 const PopupAddBill = memo(forwardRef((props, ref) => {
   const [show, setShow] = useState(false)
+  const [payType, setPayType] = useState('expense')
+  const [date, setDate] = useState(new Date())
+
+  const dateRef = useRef()
 
   useImperativeHandle(ref, () => ({
     show: () => setShow(true),
     close: () => setShow(false)
   }))
+
+
 
   return (
     <Popup
@@ -19,7 +28,28 @@ const PopupAddBill = memo(forwardRef((props, ref) => {
       destroy={false}
       mountContainer={() => document.body}
     >
-      <div style={{ height: 200, background: '#fff' }}>弹窗</div>
+      <div className={s.addWrap}>
+        <div className={s.filter}>
+          <div className={s.type}>
+            <span
+              className={classNames([s.expense], { [s.active]: payType === 'expense' })}
+              onClick={() => setPayType('expense')}
+            >
+              支出
+            </span>
+            <span
+              className={classNames([s.income], { [s.active]: payType === 'income' })}
+              onClick={() => setPayType('income')}
+            >
+              收入
+            </span>
+          </div>
+          <div className={s.time} onClick={() => dateRef.current && dateRef.current.show()}>
+            {dayjs(date).format('MM-DD')}
+          </div>
+        </div>
+        <PopupDate ref={dateRef} onSelect={(value) => setDate(value)} />
+      </div>
     </Popup>
   )
 }))
