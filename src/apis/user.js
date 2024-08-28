c5f4507049b742056127fda663e27269ed87d2bb
cheeseburgertony <1821955020@qq.com>
2024-08-28 11:57:24 +0800

    登录页面-注册和登录功能基本完成

--- src/apis/user.js
+++ src/apis/user.js
@@ -3,3 +3,5 @@ import request from "@/services";
 // 用户注册
 export const postUserRegisterAPI = (params) => request.post('/api/user/register', params)
 
+// 用户登录
+export const postUserLoginAPI = (params) => request.post('/api/user/login', params)
