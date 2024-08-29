351b49346a442dca01a9df4893149dd0f605ab26
cheeseburgertony <1821955020@qq.com>
2024-08-29 18:12:29 +0800

    feat: 用户页面-用户详情页面样式实现以及用户上传头像实现

--- src/apis/user.js
+++ src/apis/user.js
@@ -8,3 +8,15 @@ export const postUserLoginAPI = (params) => request.post('/api/user/login', para
 
 // 获取用户信息
 export const getUserGetUserInfoAPI = () => request.get('/api/user/get_userinfo')
+
+// 上传用户头像
+// 通过axios设置  'Content-Type': 'multipart/form-data', 进行文件上传
+export const postUploadAPI = (formData) =>
+  request({
+    method: 'POST',
+    url: '/api/upload',
+    data: formData,
+    headers:{
+      'Content-Type':'multipart/form-data',
+    }
+  })
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,13 +3,15 @@ import Detail from "@/pages/Detail"
 import Home from "@/pages/Home"
 import Login from "@/pages/Login"
 import User from "@/pages/User"
+import UserInfo from "@/pages/UserInfo"
 
 const routes = [
   { path: '/', element: <Home /> },
   { path: '/data', element: <Data /> },
   { path: '/user', element: <User /> },
   { path: '/detail', element: <Detail /> },
-  { path: '/login', element: <Login /> }
+  { path: '/login', element: <Login /> },
+  { path: '/userinfo', element: <UserInfo /> }
 ]
 
 export default routes
