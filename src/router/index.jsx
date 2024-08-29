65aafe130b21b26c3f67a4255b885e94789a3670
cheeseburgertony <1821955020@qq.com>
2024-08-30 01:05:07 +0800

    chore: 使用路由懒加载进行优化

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react-refresh/only-export-components */
 import About from "@/pages/About"
 import Account from "@/pages/Account"
-import Data from "@/pages/Data"
-import Detail from "@/pages/Detail"
-import Home from "@/pages/Home"
-import Login from "@/pages/Login"
-import User from "@/pages/User"
 import UserInfo from "@/pages/UserInfo"
+import { lazy } from "react"
+
+const Home = lazy(() => import('@/pages/Home'))
+const Data = lazy(() => import('@/pages/Data'))
+const User = lazy(() => import('@/pages/User'))
+const Login = lazy(() => import('@/pages/Login'))
+const Detail = lazy(() => import('@/pages/Detail'))
 
 const routes = [
   { path: '/', element: <Home /> },
