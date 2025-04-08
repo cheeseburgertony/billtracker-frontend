ca9ffd84b8ce71a85bc918b33ac1db6931e07bdb
cheeseburgertony <1821955020@qq.com>
2025-04-08 21:35:16 +0800

    refactor: 使用ts重构axios实例创建和拦截器

--- src/services/request/config.ts
+++ src/services/request/config.ts
@@ -1,5 +1,5 @@
 
-const MODE = import.meta.env.MODE // 环境变量
+const MODE = (import.meta as any).env.MODE as 'development' | 'production'; // 环境变量
 
 export const BASE_URL = MODE === 'development' ? 'http://139.159.194.91:7001' : 'http://139.159.194.91:7001'
 export const TIMEOUT = 10000
